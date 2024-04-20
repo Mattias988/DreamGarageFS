21a5d319cb0b67de86e580922e4ee0065ff5b58a
Mattias988 <maciek.korsakowski@gmail.com>
2024-04-20 15:39:50 +0200

    add entity

--- src/main/java/com/userproject/fullstackapp/model/User.java
+++ src/main/java/com/userproject/fullstackapp/model/User.java
@@ -1,4 +1,20 @@
 package com.userproject.fullstackapp.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity(name = "fullstack")
 public class User {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String username;
+    private String name;
+    private String email;
 }
