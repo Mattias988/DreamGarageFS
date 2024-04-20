ccc25fd1d080590ee862e35a014d9738eb49154f
Mattias988 <maciek.korsakowski@gmail.com>
2024-04-20 15:49:56 +0200

    add user controller

--- src/main/java/com/userproject/fullstackapp/controller/UserController.java
+++ src/main/java/com/userproject/fullstackapp/controller/UserController.java
@@ -1,4 +1,22 @@
 package com.userproject.fullstackapp.controller;
 
+
+import com.userproject.fullstackapp.model.User;
+import com.userproject.fullstackapp.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    //add user controller
+    @PostMapping("/user")
+    User newUser(@RequestBody User newUser){
+        return userRepository.save(newUser);
+    }
 }
