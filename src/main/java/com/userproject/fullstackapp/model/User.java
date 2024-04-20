634807f41a14024d7ac4f3cd7782dd156c269e55
Jakub <jakub.anuzlewicz@onet.pl>
2024-04-21 00:17:23 +0200

    add annotations

--- src/main/java/com/userproject/fullstackapp/model/User.java
+++ src/main/java/com/userproject/fullstackapp/model/User.java
@@ -1,18 +1,22 @@
 package com.userproject.fullstackapp.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Table
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity(name = "fullstack")
 public class User {
 
     @Id
     @GeneratedValue
+    @Column(name = "id")
     private Long id;
     private String username;
     private String name;
