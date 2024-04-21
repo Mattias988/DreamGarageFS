322f92cc9bc18e61464c13806213b65af7a5ce5a
Mattias988 <maciek.korsakowski@gmail.com>
2024-04-21 13:41:15 +0200

    add annotations to DTO

--- src/main/java/com/userproject/fullstackapp/DTO/UserDTO.java
+++ src/main/java/com/userproject/fullstackapp/DTO/UserDTO.java
@@ -1,8 +1,15 @@
 package com.userproject.fullstackapp.DTO;
 
-public class UserDTO {
-    private String newName;
-    private String newUsername;
-    private String newEmail;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
+public class UserDTO {
+
+    private String newName;
+
+    private String newUsername;
+
+    private String newEmail;
 }
