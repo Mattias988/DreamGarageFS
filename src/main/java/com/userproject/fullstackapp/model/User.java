709726720c77d12c751ccb9fe2fdbe28d0a3ec39
Mattias988 <maciek.korsakowski@gmail.com>
2024-04-21 13:40:37 +0200

    add annotations

--- src/main/java/com/userproject/fullstackapp/model/User.java
+++ src/main/java/com/userproject/fullstackapp/model/User.java
@@ -1,6 +1,7 @@
 package com.userproject.fullstackapp.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -18,7 +19,14 @@ public class User {
     @GeneratedValue
     @Column(name = "id")
     private Long id;
+
+    @NotBlank
     private String username;
+
+    @NotBlank
     private String name;
+
+    @NotBlank
     private String email;
+
 }
