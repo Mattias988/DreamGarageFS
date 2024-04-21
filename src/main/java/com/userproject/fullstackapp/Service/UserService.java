c4ec063f08f6ae62de9e4ad511c447a497da01e8
Mattias988 <maciek.korsakowski@gmail.com>
2024-04-21 13:41:30 +0200

    add corrections to update method

--- src/main/java/com/userproject/fullstackapp/Service/UserService.java
+++ src/main/java/com/userproject/fullstackapp/Service/UserService.java
@@ -3,6 +3,7 @@ package com.userproject.fullstackapp.Service;
 import com.userproject.fullstackapp.DTO.UserDTO;
 import com.userproject.fullstackapp.model.User;
 import com.userproject.fullstackapp.repository.UserRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,17 +11,18 @@ import java.util.Optional;
 
 @Service
 public class UserService {
+
     @Autowired
     private UserRepository userRepository;
 
-//    public User updateUser(Long userId, UserDTO userDTO){
-//        Optional<User> updateUser = Optional.ofNullable(userRepository.findById(userId)
-//                .orElseThrow(() -> {
-//                    return new RuntimeException("Nie istnieje");
-//                }));
-//
-//
-//    }
+    public User updateUser(Long userId, UserDTO userDTO){
+        User existingUser = userRepository.findById(userId)
+                .orElseThrow(() -> new EntityNotFoundException("Ten użytkownik nie istnieje!"));
+        existingUser.setName(userDTO.getNewName());
+        existingUser.setUsername(userDTO.getNewUsername());
+        existingUser.setEmail(userDTO.getNewEmail());
+        return userRepository.save(existingUser);
+    }
 
 
 }
