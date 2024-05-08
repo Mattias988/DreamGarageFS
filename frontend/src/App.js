1293fa11efe625e9e1067cc6c9bb8cfdb88dd0cc
Mattias988 <maciek.korsakowski@gmail.com>
2024-05-08 12:06:20 +0200

    add new page to root

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import Nav from './components/Nav';
 import About from './pages/About';
 import Home from './pages/Home';
 import {BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Shop from './pages/Shop';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Routes>
             <Route exact path='/' element={<Home />} />
             <Route exact path='/about' element={<About />} />
+            <Route exact path='/shop' element={<Shop />} />
           </Routes>
         </div>
       </div>
