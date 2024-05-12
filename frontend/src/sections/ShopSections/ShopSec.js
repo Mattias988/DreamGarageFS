ff5d5b8c05619f87a488f869b17f88bf11a01ae8
MKorsakowsky <maciek.korsakowski@gmail.com>
2024-05-12 18:22:13 +0200

    add icons and products

--- frontend/src/sections/ShopSections/ShopSec.js
+++ frontend/src/sections/ShopSections/ShopSec.js
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from 'react'
+import Product from '../../components/Product';
 
 function ShopSec() {
 
     const[data, setData] = useState(null);
-    const[error, setError] = useState(null);
 
     useEffect(() => {
         fetch('http://localhost:8080/products')
@@ -18,7 +18,7 @@ function ShopSec() {
             })
     }, [])
 
-    return (
+    return ( 
       <section className='bg-stone-800 w-full h-fit'>
         <div className='grid justify-items-center pb-10'>
             <span className='grid justify-items-center text-white font-primary pt-32 pb-24'>
@@ -30,24 +30,9 @@ function ShopSec() {
                 </h1>
             </span>
             <div className='bg-tertiary w-10/12 h-fit rounded-2xl grid grid-cols-3 gap-10 py-10 justify-items-center'>
-                <div className='w-[356px] h-[389px] rounded-2xl bg-white '>
-
-                </div>
-                <div className='w-[356px] h-[389px] rounded-2xl bg-white '>
-                    
-                </div>
-                <div className='w-[356px] h-[389px] rounded-2xl bg-white'>
-                    
-                </div>
-                <div className='w-[356px] h-[389px] rounded-2xl bg-white '>
-                    
-                </div>
-                <div className='w-[356px] h-[389px] rounded-2xl bg-white'>
-                    
-                </div>
-                <div className='w-[356px] h-[389px] rounded-2xl bg-white'>
-                    
-                </div>
+                {data?.map((products) => {
+                    return <Product product={products} key={products.productId}/>
+                })}
             </div>
           </div>
       </section>
---
+++ src/main/java/com/userproject/fullstackapp/controller/Config/CorsConfig.java
@@ -0,0 +1,17 @@
+package com.userproject.fullstackapp.controller.Config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*") // Ustawia wszystkie źródła na dozwolone. Możesz określić konkretne źródła.
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // Dozwolone metody HTTP.
+                .allowedHeaders("*"); // Dozwolone nagłówki.
+    }
+}
